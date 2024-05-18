2e8e7a8656f11e2c5861450eb21fa4711da16281
rayanserratine <rayan.serratine@gmail.com>
2024-05-18 13:41:35 -0700

    why

---
+++ git.js
@@ -0,0 +1,18 @@
+let why = ''
+
+function question(){
+  why = prompt('Você ama a meg?')
+    alert('Resposta: ' + why)
+}
+
+function ryesult(){
+    if(wh == 'tetuda'){
+        alert('Nao eh amor');
+    } else {
+        alert('eh amor')
+    }
+    }
+
+
+question()
+result()
